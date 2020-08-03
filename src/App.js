7eaa18d887c7232b19864886cec4ceec19e35524
nate745 <nate745@gmail.com>
2020-08-02 21:15:14 -0500

    Add Person folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a react app</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react app</h1>
+        <p>This is really working!</p>
+        <Person />
+        <Person />
+        <Person />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
 }
 
