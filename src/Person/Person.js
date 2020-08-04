82473f0663c3323a0f135e78a01e2df2f94310ed
nate745 <nate745@gmail.com>
2020-08-03 21:56:46 -0500

    State and Props

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const person = () => {
+const person = (props) => {
     return (
-        <p>I'm a Person!</p>
+        <div>
+            <p>I'm a {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>
     )
 }
 
-export default person;
+export default person; 
